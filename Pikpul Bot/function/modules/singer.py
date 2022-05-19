58f665cfad5194de7aac3525199164e881babee0
Herothor <92557993+Herothor@users.noreply.github.com>
2022-05-19 15:15:41 +0530

    Create singer.py

---
+++ Pikpul Bot/function/modules/singer.py
@@ -0,0 +1,31 @@
+"""
+command: .singer singer name - song name 
+by @Nikhil_LoGo_MaKer
+"""
+from PyLyrics import *
+from uniborg.util import pikpul_on_cmd
+
+
+@pikpul.on(pikpul_on_cmd(pattern="singer (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+
+    input_str = event.pattern_match.group(1)
+
+    try:
+        song = input_str.split("-")
+        if len(song) == 1:
+            await event.edit("Usage: .singer Duman - Haberin Yok Ölüyorum")
+        else:
+            await event.edit("🔍︎Searching lyrics By pikpul")
+            lyrics = PyLyrics.getLyrics(song[0].strip(), song[1].strip()).split("\n")
+            lyric_message = f"Singing {song[0].strip()} from {song[1].strip()} 🎙"
+            lyric_message += "\n\n" + "\n".join(lyrics)
+            try:
+                await event.edit(lyric_message)
+            except:
+                # TODO: send as file
+                logger.info(lyric_message)
+    except ValueError:
+        await event.edit("Song not found")
